2b7488043c4ca1b48c0cd7e28a34cf9394983c1d
Silas Reis <silas.1992@live.com>
2019-02-19 09:54:03 -0300

    Criação da Segunda Conta

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -8,6 +8,15 @@ public class CriaConta {
 		//é utilizado ponto para emendar as duas variaveis.
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("A primeira conta tem " + primeiraConta.saldo);
+		System.out.println("A segunda conta tem " + segundaConta.saldo);
 	}
 
 }
