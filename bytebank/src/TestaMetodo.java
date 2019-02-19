4e1a755ef3b0d1dc33fe8db59905f4deacffded3
Silas Reis <silas.1992@live.com>
2019-02-19 11:52:50 -0300

    Utilizando retorno em métodos

--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -4,8 +4,10 @@ public class TestaMetodo {
 		Conta contaDoPaulo = new Conta();
 		contaDoPaulo.saldo = 100;
 		contaDoPaulo.deposita(50);
-		
 		System.out.println(contaDoPaulo.saldo);
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 
 }
